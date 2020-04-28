c2a19dba34c8722f0f5935fb11b2046f84fd707d
Vladyslav Uslystyi <vladuvi23@gmail.com>
2020-04-28 14:59:33 +0300

    cosmetic changes in bucketSort

--- BucketSort/BucketSort.hpp
+++ BucketSort/BucketSort.hpp
@@ -1,86 +1,101 @@
 #ifndef BUCKET_SORT  // NOLINT
 #define BUCKET_SORT
 
+#include <algorithm>
 #include <cmath>
 #include <iostream>
 #include <vector>
 
-class BucketSort {
-  size_t _min;
-  size_t _max;
+static double *partition(double *start, double *end) {
+  bool go_forward = true;
 
-  void setMinMax(double *array, size_t size) {
-    double tmp_min = __DBL_MAX__;
-    double tmp_max = 0.0;
-
-    for (size_t i = 0; i < size; i++) {
-      tmp_min = std::min(tmp_min, array[i]);  // NOLINT
-      tmp_max = std::max(tmp_max, array[i]);  // NOLINT
-    }
-    _min = static_cast<size_t>(floor(tmp_min));
-    _max = static_cast<size_t>(floor(tmp_max));
-  }
-
-  double *partition(double *start, double *end) {
-    bool go_forward = true;
-
-    while (start < end) {
-      if (*start > *end) {
-        std::swap(*start, *end);
-        go_forward ? end-- : start++;  // NOLINT
-        go_forward = !go_forward;
-      } else {
-        go_forward ? start++ : end--;  // NOLINT
-      }
-    }
-    return end;
-  }
-
-  void mQsort(double *start, double *end) {
-    double *center = partition(start, end);
-
-    if (start != center) {
-      mQsort(start, center - 1); // NOLINT
-    }
-    if (end != center) {
-      mQsort(center + 1, end); // NOLINT
+  while (start < end) {
+    if (*start > *end) {
+      std::swap(*start, *end);
+      go_forward ? end-- : start++;  // NOLINT
+      go_forward = !go_forward;
+    } else {
+      go_forward ? start++ : end--;  // NOLINT
     }
   }
+  return end;
+}
+
+static void mQsort(double *start, double *end) {
+  double *center = partition(start, end);
+
+  if (start != center) {
+    mQsort(start, center - 1); // NOLINT
+  }
+  if (end != center) {
+    mQsort(center + 1, end); // NOLINT
+  }
+}
+
+class BucketSorter {
+  
+  size_t              _max_collisions{0};
+  std::vector<double> _vector;
 
  public:
-  size_t operator()(double *array, size_t size) {  // NOLINT
-    if (array == nullptr || size == 0u) {
-      return 0;
+
+  void startSort() {
+
+    if (_vector.empty()) {
+      _max_collisions = 0;
+      return ;
     }
 
-    setMinMax(array, size);
-
-    size_t count_buckets = _max - _min + 1;
+    size_t min = static_cast<size_t>(floor(*std::min_element(_vector.begin(), _vector.end())));
+    size_t max = static_cast<size_t>(floor(*std::max_element(_vector.begin(), _vector.end())));
+    size_t count_buckets = max - min + 1;
     std::vector<std::vector<double>> buckets;
-		
-		buckets.resize(count_buckets);
+    
+    buckets.resize(count_buckets);
 
-    for (size_t i = 0; i < size; i++) {
-      // NOLINTNEXTLINE
-      buckets[static_cast<size_t>(floor(array[i])) - _min].push_back(array[i]);
+    for (double num : _vector) {
+      buckets[static_cast<size_t>(floor(num)) - min].push_back(num);
     }
 
-    size_t max_collision = 0;
     size_t index = 0;
+    _max_collisions = 0;
     for (size_t i = 0; i < count_buckets; i++) {
       if (!buckets[i].empty()) {
         double *tmp_arr = buckets[i].data();
 
         mQsort(tmp_arr, tmp_arr + (buckets[i].size() - 1));  // NOLINT
-        max_collision = std::max(max_collision, buckets[i].size());
+        _max_collisions = std::max(_max_collisions, buckets[i].size());
       }
 			// NOLINTNEXTLINE
       for (size_t j = 0; j < buckets[i].size(); j++) {
-        array[index++] = buckets[i][j];  // NOLINT
+        _vector[index++] = buckets[i][j];  // NOLINT
       }
     }
+  }
 
-    return max_collision;
+  void push(double elem) {
+    _vector.push_back(elem);
+  }
+
+  size_t getMaxCollisions() const {
+    return _max_collisions;
+  }
+
+  std::vector<double> getVector() const {
+    return _vector;
+  }
+
+  void printVector() const {
+    for (size_t i = 0; i < _vector.size(); i++) {
+      if (i != 0u) { std::cout << " "; }
+      std::cout << _vector[i];
+    }
+    std::cout << std::endl;
+  }
+
+  void printFormat() const {
+    printVector();
+    std::cout << _max_collisions << std::endl;
   }
 };
 
